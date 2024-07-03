7b91444e2f3272920fdfe58093e52d6f27afda02
Gautammangesh <129382154+Gautammangesh@users.noreply.github.com>
2024-07-03 19:45:41 +0530

    Add files via upload

---
+++ src/components/createTaskForm/_taskDateField.tsx
@@ -0,0 +1,21 @@
+import React, { FC, ReactElement, useState } from 'react';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+
+export const TaskDateField: FC = (): ReactElement => {
+  // state
+  const [date, setDate] = useState<Date | null>(null);
+  return (
+    <>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <DesktopDatePicker
+          label="Task Date"
+          format="dd/MM/yyyy"
+          value={date}
+          onChange={(newValue) => setDate(newValue)}
+        />
+      </LocalizationProvider>
+    </>
+  );
+};
